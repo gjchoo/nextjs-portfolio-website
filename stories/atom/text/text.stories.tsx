9fc0402aa2eaffa24813e88f2275e586f2aa26da
gjchoo <gjchoo@icloud.com>
2022-05-12 23:35:55 +0800

    feat: Add text atomic component

---
+++ stories/atom/text/text.stories.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import { Text, WeightTypes, SizeTypes } from "./text";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: "Atoms/Text",
+  component: Text,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    weight: {
+      options: [0, 1, 2, 3], // iterator
+      mapping: ["light", "regular", "medium", "semibold"], // values
+      control: {
+        type: "select",
+        labels: ["light", "regular", "medium", "semibold"],
+      },
+    },
+    size: {
+      options: [0, 1, 2, 3, 4], // iterator
+      mapping: ["fs14", "fs18", "fs24", "fs48", "fs60"], // values
+      control: {
+        type: "select",
+        labels: ["fs14", "fs18", "fs24", "fs48", "fs60"],
+      },
+    },
+    color: {
+      options: [0, 1], // iterator
+      mapping: ["primary", "secondary"], // values
+      control: {
+        type: "select",
+        labels: ["primary", "secondary"],
+      },
+    },
+  },
+} as ComponentMeta<typeof Text>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Text> = (args) => <Text {...args} />;
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {
+  text: "This is a sample of a paragraph text",
+  weight: WeightTypes.light,
+  size: SizeTypes.fs18,
+  lineHeight: 29,
+  color: "secondary",
+};
---
+++ stories/atom/text/text.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import styles from "./Text.module.css";
+
+export enum WeightTypes {
+  semibold = "semibold",
+  medium = "medium",
+  regular = "regular",
+  light = "light",
+}
+
+export enum SizeTypes {
+  fs60 = "fs60",
+  fs48 = "fs48",
+  fs24 = "fs24",
+  fs18 = "fs18",
+  fs14 = "fs14",
+}
+
+export interface TextProps {
+  /**
+   * text contents
+   */
+  text: string;
+  /**
+   * size of the text
+   */
+  size: SizeTypes;
+  /**
+   * size of the text
+   */
+  weight: WeightTypes;
+  /**
+   * lineHeight of the text
+   */
+  lineHeight?: number;
+  /**
+   * color of the text
+   */
+  color?: string;
+}
+
+/**
+ * Primary UI component for user interaction
+ */
+export const Text = ({
+  text,
+  size,
+  weight,
+  lineHeight = 29,
+  color = "secondary",
+}: TextProps) => {
+  return (
+    <p
+      data-testid="storybook-p"
+      className={[text, weight, size, color].join(" ")}
+      style={{ lineHeight: `${lineHeight}px`, color }}
+    >
+      {text}
+    </p>
+  );
+};
