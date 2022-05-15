7964599954ac32c9b059e859c5d2bc1ef7e736f8
gjchoo <gjchoo@icloud.com>
2022-05-15 16:01:27 +0800

    refactor: added security header
    modified next config file & _documents.tsx

--- next.config.js
+++ next.config.js
@@ -1,6 +1,39 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self';
+  child-src gjchoo.com;
+  style-src 'self' gjchoo.com;
+  font-src 'self';  
+`;
 
-module.exports = nextConfig;
+const securityHeaders = [
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=63072000; includeSubDomains; preload",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "SAMEORIGIN",
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  // {
+  //   key: "Content-Security-Policy",
+  //   value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim(),
+  // },
+];
+
+module.exports = {
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: "/:path*",
+        headers: securityHeaders,
+      },
+    ];
+  },
+};
