485246ea4c0d56eeb0a80c392381dad69337b0ed
gjchoo <gjchoo@icloud.com>
2022-05-17 22:54:44 +0800

    refactor: Added react pdf
    use another library for the react pdf and refactor using js instead of tsx

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -1,21 +1,9 @@
 import dynamic from "next/dynamic";
 
-//@ts-ignore
-const FileViewer = dynamic(() => import("react-file-viewer"), {
+const PDFViewer = dynamic(() => import("../components/pdf-viewer"), {
   ssr: false,
 });
 
-export default function Index() {
-  return (
-    <div
-      style={{
-        backgroundColor: "white",
-        // display: "flex",
-        // justifyContent: "center",
-        // alignItems: "center",
-      }}
-    >
-      <FileViewer fileType="pdf" filePath="/resume_gjchoo.pdf" />
-    </div>
-  );
+export default function PDF() {
+  return <PDFViewer />;
 }
